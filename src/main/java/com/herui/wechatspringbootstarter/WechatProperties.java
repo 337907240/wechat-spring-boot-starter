4a41623b01b427c494940e304d9eefc6d8428105
何锐 <337907240@qq.com>
2020-06-30 20:05:19 +0800

    默认值处理

--- src/main/java/com/herui/wechatspringbootstarter/WechatProperties.java
+++ src/main/java/com/herui/wechatspringbootstarter/WechatProperties.java
@@ -1,9 +1,199 @@
 package com.herui.wechatspringbootstarter;
 
+import me.chanjar.weixin.common.bean.WxAccessToken;
+import me.chanjar.weixin.common.util.ToStringUtils;
+import me.chanjar.weixin.common.util.http.apache.ApacheHttpClientBuilder;
+import me.chanjar.weixin.cp.config.WxCpConfigStorage;
 import me.chanjar.weixin.cp.config.WxCpInMemoryConfigStorage;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import sun.management.resources.agent;
+
+import java.io.File;
 
 @ConfigurationProperties("wx.corp")
-public class WechatProperties extends WxCpInMemoryConfigStorage{
+public class WechatProperties implements WxCpConfigStorage {
+    protected volatile String corpId;
+    protected volatile String corpSecret;
+    protected volatile String token;
+    protected volatile String accessToken;
+    protected volatile String aesKey;
+    protected volatile Integer agentId;
+    protected volatile long expiresTime;
+    protected volatile String oauth2redirectUri;
+    protected volatile String httpProxyHost;
+    protected volatile int httpProxyPort;
+    protected volatile String httpProxyUsername;
+    protected volatile String httpProxyPassword;
+    protected volatile String jsapiTicket;
+    protected volatile long jsapiTicketExpiresTime;
+    protected volatile File tmpDirFile;
+    private volatile ApacheHttpClientBuilder apacheHttpClientBuilder;
 
+    public WechatProperties() {
+        this.corpId ="ww33c32b174b11bfe2";
+        this.corpSecret ="lAOoEseXx8zSRIZYcGpe2D3GTCUpKT86YFmRKrcR-hE";
+        this.agentId =1000003;
+    }
+
+    public String getAccessToken() {
+        return this.accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public boolean isAccessTokenExpired() {
+        return System.currentTimeMillis() > this.expiresTime;
+    }
+
+    public void expireAccessToken() {
+        this.expiresTime = 0L;
+    }
+
+    public synchronized void updateAccessToken(WxAccessToken accessToken) {
+        this.updateAccessToken(accessToken.getAccessToken(), accessToken.getExpiresIn());
+    }
+
+    public synchronized void updateAccessToken(String accessToken, int expiresInSeconds) {
+        this.accessToken = accessToken;
+        this.expiresTime = System.currentTimeMillis() + (long) (expiresInSeconds - 200) * 1000L;
+    }
+
+    public String getJsapiTicket() {
+        return this.jsapiTicket;
+    }
+
+    public void setJsapiTicket(String jsapiTicket) {
+        this.jsapiTicket = jsapiTicket;
+    }
+
+    public long getJsapiTicketExpiresTime() {
+        return this.jsapiTicketExpiresTime;
+    }
+
+    public void setJsapiTicketExpiresTime(long jsapiTicketExpiresTime) {
+        this.jsapiTicketExpiresTime = jsapiTicketExpiresTime;
+    }
+
+    public boolean isJsapiTicketExpired() {
+        return System.currentTimeMillis() > this.jsapiTicketExpiresTime;
+    }
+
+    public synchronized void updateJsapiTicket(String jsapiTicket, int expiresInSeconds) {
+        this.jsapiTicket = jsapiTicket;
+        this.jsapiTicketExpiresTime = System.currentTimeMillis() + (long) (expiresInSeconds - 200) * 1000L;
+    }
+
+    public void expireJsapiTicket() {
+        this.jsapiTicketExpiresTime = 0L;
+    }
+
+    public String getCorpId() {
+        return this.corpId;
+    }
+
+    public void setCorpId(String corpId) {
+        this.corpId = corpId;
+    }
+
+    public String getCorpSecret() {
+        return this.corpSecret;
+    }
+
+    public void setCorpSecret(String corpSecret) {
+        this.corpSecret = corpSecret;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public long getExpiresTime() {
+        return this.expiresTime;
+    }
+
+    public void setExpiresTime(long expiresTime) {
+        this.expiresTime = expiresTime;
+    }
+
+    public String getAesKey() {
+        return this.aesKey;
+    }
+
+    public void setAesKey(String aesKey) {
+        this.aesKey = aesKey;
+    }
+
+    public Integer getAgentId() {
+        return this.agentId;
+    }
+
+    public void setAgentId(Integer agentId) {
+        this.agentId = agentId;
+    }
+
+    public String getOauth2redirectUri() {
+        return this.oauth2redirectUri;
+    }
+
+    public void setOauth2redirectUri(String oauth2redirectUri) {
+        this.oauth2redirectUri = oauth2redirectUri;
+    }
+
+    public String getHttpProxyHost() {
+        return this.httpProxyHost;
+    }
+
+    public void setHttpProxyHost(String httpProxyHost) {
+        this.httpProxyHost = httpProxyHost;
+    }
+
+    public int getHttpProxyPort() {
+        return this.httpProxyPort;
+    }
+
+    public void setHttpProxyPort(int httpProxyPort) {
+        this.httpProxyPort = httpProxyPort;
+    }
+
+    public String getHttpProxyUsername() {
+        return this.httpProxyUsername;
+    }
+
+    public void setHttpProxyUsername(String httpProxyUsername) {
+        this.httpProxyUsername = httpProxyUsername;
+    }
+
+    public String getHttpProxyPassword() {
+        return this.httpProxyPassword;
+    }
+
+    public void setHttpProxyPassword(String httpProxyPassword) {
+        this.httpProxyPassword = httpProxyPassword;
+    }
+
+    public String toString() {
+        return ToStringUtils.toSimpleString(this);
+    }
+
+    public File getTmpDirFile() {
+        return this.tmpDirFile;
+    }
+
+    public void setTmpDirFile(File tmpDirFile) {
+        this.tmpDirFile = tmpDirFile;
+    }
+
+    public ApacheHttpClientBuilder getApacheHttpClientBuilder() {
+        return this.apacheHttpClientBuilder;
+    }
+
+    public void setApacheHttpClientBuilder(ApacheHttpClientBuilder apacheHttpClientBuilder) {
+        this.apacheHttpClientBuilder = apacheHttpClientBuilder;
+    }
 }
